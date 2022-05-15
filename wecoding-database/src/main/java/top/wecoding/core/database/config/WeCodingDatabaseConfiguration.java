5bd90d87ef98a2c101f56ad0587c9f273acf8de1
Liuyuhui <1515418211@qq.com>
2022-05-15 15:57:40 +0800

    feat: boot启动模块增加通用配置

---
+++ wecoding-database/src/main/java/top/wecoding/core/database/config/WeCodingDatabaseConfiguration.java
@@ -0,0 +1,12 @@
+package top.wecoding.core.database.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liuyuhui
+ * @date 2022/5/15
+ * @qq 1515418211
+ */
+@Configuration(proxyBeanMethods = false)
+public class WeCodingDatabaseConfiguration {
+}
