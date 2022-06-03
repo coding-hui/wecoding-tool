394fcbf031fba964b5c39bcf03a2cca17bee4a36
Liuyuhui <1515418211@qq.com>
2022-06-03 11:25:53 +0800

    docs: 添加版权

--- wecoding-auto/src/main/java/top/wecoding/core/auto/annotation/AutoIgnore.java
+++ wecoding-auto/src/main/java/top/wecoding/core/auto/annotation/AutoIgnore.java
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2019-2029, Dreamlu 卢春梦 (596392912@qq.com & www.dreamlu.net).
- * <p>
+ * Copyright (c) 2019-2022. WeCoding (wecoding@yeah.net).
+ *
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.gnu.org/licenses/lgpl.html
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- wecoding-core/src/main/java/top/wecoding/core/util/ClassUtil.java
+++ wecoding-core/src/main/java/top/wecoding/core/util/ClassUtil.java
@@ -1,12 +1,12 @@
-/**
- * Copyright (c) 2018-2028, DreamLu 卢春梦 (qq596392912@gmail.com).
- * <p>
+/*
+ * Copyright (c) 2018-2022. WeCoding (wecoding@yeah.net).
+ *
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.gnu.org/licenses/lgpl.html
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
